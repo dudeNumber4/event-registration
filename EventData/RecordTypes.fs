cf1db5cb2d1de8bc92dd491a8efcca4d0c712b31
Brad Wood <bwood@chathamfinancial>
2020-08-17 10:24:52 -0600

    Added csv to source for now

--- EventData/RecordTypes.fs
+++ EventData/RecordTypes.fs
@@ -2,19 +2,22 @@
 
 module RecordTypes =
 
+  // id, Day, Title, Description
+  type SessionRecord = (string * string * string * string)
+  let sessionFileName = "Session.csv"
+
   // id, firstName, LastName, Email, OrgName, Industry
   type RegistrantRecord = (string * string * string * string * string * string)
   let registrantFileName = "Registrant.csv"
-
-  // id, registrationId, unbounded SessionIds
-  type ItineraryRecord = (string * string * string List)
-  let itineraryFileName = "Itinerary.csv"
 
   // id, RegistrantId
   type RegistrationRecord = (string * string)
   let registrationFileName = "Registration.csv"
 
-  // id, Day, Title, Description
-  type SessionRecord = (string * string * string * string)
-  let sessionFileName = "Session.csv"
+  // id, registrationId, unbounded SessionIds
+  type ItineraryRecord = (string * string * string List)
+  let itineraryFileName = "Itinerary.csv"
 
+// Itinerary -> Registration + (sessions)
+// Registration -> Registrant + (I think I originally meant to add some kind of flag that the registrant has read the waiver or something)
+// Registrant (personal info)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # event-registration
 Proof of concept of a Blazor application
+
+* The data is simply sitting in Session.csv at the root.
+* Set a breakpoint in EditSession.razor, launch, go to "Sessions" in the nav, and click Edit.
